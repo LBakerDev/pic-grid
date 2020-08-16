c4de9241883c74070f4bd4df16ad37510950e76e
naeroled <londonbaker4@gmail.com>
2020-08-16 13:19:45 -0400

    Updates

---
+++ precache-manifest.fff5aef844824649b33342dd022b4269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f4bf68b9a4c9f5349f9e062fb0f176",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ae6d9df52f87d2bd0e6",
+    "url": "/static/css/main.8114aaa0.chunk.css"
+  },
+  {
+    "revision": "8c842e53307be467d880",
+    "url": "/static/js/2.0eaaeac4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0eaaeac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ae6d9df52f87d2bd0e6",
+    "url": "/static/js/main.0743ab0a.chunk.js"
+  },
+  {
+    "revision": "12da6d54b81111279a24",
+    "url": "/static/js/runtime-main.a80a271f.js"
+  }
+]);
